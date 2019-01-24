f1df9bd58d024fadbc245581f040409e72f7e003
Sergey <xxsmotur@gmail.com>
2019-01-24 17:16:13 +0500

    Add query struct

---
+++ queries.go
@@ -0,0 +1,10 @@
+package metrics
+
+// Query provides struct for the query definition
+type Query struct {
+	Entity    string
+	Label     string
+	TsEqual   uint32
+	Tsgreater uint32
+	TsLess    uint
+}
