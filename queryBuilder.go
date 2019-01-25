9a50a16d649de9dfd0ee1ff3d81939e26c93340b
Sergey <xxsmotur@gmail.com>
2019-01-25 18:43:42 +0500

    moving of method for makeEntitiesQuery to queryBuilder

--- queryBuilder.go
+++ queryBuilder.go
@@ -18,9 +18,24 @@ func (q *queryBuilder) make() (string, error) {
 	}
 	queryReq := fmt.Sprintf("SELECT %s(values[indexOf(names, '%s')]) AS result FROM %s", action, q.aq.Label, q.c.DBName)
 	if len(q.aq.Entities) > 0 {
-		queryReq += q.aq.makeEntitiesQuery()
+		queryReq += q.makeEntitiesQuery()
 	}
 	return queryReq, nil
+}
+
+func (q *queryBuilder) makeEntitiesQuery() string {
+	entities := q.aq.Entities
+	if len(entities) == 1 {
+		return fmt.Sprintf(" WHERE entity = '%s'", entities[0])
+	}
+	res := " WHERE ( "
+	for i := 0; i < len(entities); i++ {
+		res += fmt.Sprintf("entity = '%s' ", entities[i])
+		if i+1 != len(entities) {
+			res += "OR "
+		}
+	}
+	return res + ")"
 }
 
 // checkAction return error if action function is not defined
